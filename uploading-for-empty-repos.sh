103c424c3ea0e2cd3bbd6ef3bfe696e9e4f2f6eb
Crystyano Almeida <111809@pmenos.com.br>
2021-05-28 01:26:43 -0300

    first commit

---
+++ uploading-for-empty-repos.sh
@@ -0,0 +1,7 @@
+echo "# AngularCodes" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/WRCrys/AngularCodes.git
+git push -u origin main
