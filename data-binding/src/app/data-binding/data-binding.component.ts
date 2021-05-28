e7a84b356e51c90a6a5665dabbca828e10449f6f
Crystyano Almeida <111809@pmenos.com.br>
2021-05-28 01:56:07 -0300

    sending the second project

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Alert {
+  type: string;
+  message: string;
+}
+
+const ALERTS: Alert[] = [{
+    type: 'success',
+    message: 'This is an success alert',
+  }, {
+    type: 'info',
+    message: 'This is an info alert',
+  }, {
+    type: 'warning',
+    message: 'This is a warning alert',
+  }, {
+    type: 'danger',
+    message: 'This is a danger alert',
+  }, {
+    type: 'primary',
+    message: 'This is a primary alert',
+  }, {
+    type: 'secondary',
+    message: 'This is a secondary alert',
+  }, {
+    type: 'light',
+    message: 'This is a light alert',
+  }, {
+    type: 'dark',
+    message: 'This is a dark alert',
+  }
+];
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss']
+})
+
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'http://github.com/WRCrys';
+  cursoAngular: boolean = true;
+  urlImage: string = 'http://lorempixel.com.br/500/400/?2';
+  alerts: Alert[];
+
+  getValor() {
+    return 3;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() {
+    this.alerts = ALERTS;
+   }
+
+  close(alert: Alert) {
+    this.alerts.splice(this.alerts.indexOf(alert), 1);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
